4a13ffa026a2f41531023174c2ef8b72078c4d24
langtucodon007 <luuduchunghmd1619@gmail.com>
2021-06-16 10:40:57 +0700

    Backend submission
    Finish storeowner feedback feature

--- GroceryStoreManagement/web/storeowner/js/feedback-storeowner.js
+++ GroceryStoreManagement/web/storeowner/js/feedback-storeowner.js
@@ -1,4 +1,5 @@
-function GetFeedbackList(){
+// Function triggered when page first load
+function GetAllFeedbackList(){
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState >= 4 && this.status <= 200) {
@@ -7,9 +8,56 @@ function GetFeedbackList(){
             processFeedback(feedbackObject);
         }
     };
+    
     xhttp.open("GET","GetFeedbackList", true);
     xhttp.send();
 }
+
+// Function triggered when button "mark as read" is clicked
+function GetAllFeedbackListFromButton(button){
+    var xhttp = new XMLHttpRequest();
+    var feedback_ID = button.getAttribute("data-feedbackID")
+    xhttp.onreadystatechange = function () {
+        if (this.readyState >= 4 && this.status <= 200) {
+            console.log(this.responseText);
+            var feedbackObject = JSON.parse(this.responseText);
+            processFeedback(feedbackObject);
+        }
+    };
+    
+    if (feedback_ID == null){
+        var url = "GetFeedbackList";
+    } else {
+        var url = "GetFeedbackList?feedback_ID=" + feedback_ID;
+    }
+    
+    xhttp.open("GET",url, true);
+    xhttp.send();
+}
+
+// Function triggered when checkbox "Display read feedback" is check
+function displayFeedback(checkboxFeedback){
+    if (checkboxFeedback.checked){
+        GetSeenFeedbackList();
+    } else {
+        GetAllFeedbackList();
+    }
+}
+    
+function GetSeenFeedbackList(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState >= 4 && this.status <= 200) {
+            console.log(this.responseText);
+            var feedbackObject = JSON.parse(this.responseText);
+            processFeedback(feedbackObject);
+        }
+    };
+    
+    xhttp.open("GET","SeenFeedback", true);
+    xhttp.send();
+}
+
 
 function processFeedback(data){
     // get and clear table body
@@ -32,11 +80,20 @@ function processFeedback(data){
         cell1.innerHTML = count;
         cell2.innerHTML = data[i].feedback_date;
         cell3.innerHTML = data[i].feedback_content;
-        cell4.innerHTML = data[i].cashier_username;
-        cell5.innerHTML = "<button onclick='SeenFeedback()'>Mark as read</button>";
+        cell4.innerHTML = data[i].account.name;
+        cell5.innerHTML = "<button onclick='GetAllFeedbackListFromButton(this)' data-feedbackID="+ data[i].feedback_ID +">Mark as read</button>";
+        
+        // append disabled class and remove button if the feedback is seen
+        if (data[i].is_seen == 1 ){
+            row.className = "seen-feedback";
+            cell5.innerHTML = "";
+        }
     }
-}
-
-function SeenFeedback(){
     
 }
+
+
+
+
+
+
