ce868cc213ea1c6b1f83c787d6dc1f8e32a3d536
Thanh Hoang Phuoc <phuocthanhqt113@gmail.com>
2021-07-24 22:16:30 +0700

    Update cashier

--- GroceryStoreManagement/web/cashier/js/cashierDashboard.js
+++ GroceryStoreManagement/web/cashier/js/cashierDashboard.js
@@ -477,6 +477,7 @@ function searchCustomerByPhone() {
       result_dto = JSON.parse(this.responseText);
       if (result_dto != null) {
         currentBill.customer_dto = result_dto;
+        document.getElementById("discount-checkbox").classList.remove("d-none");
         printBill(currentBill);
       } else if (result_dto == null) {
         $("#fail-to-find-customer-toast").toast({
@@ -507,6 +508,7 @@ function renderCustomer() {
     document.getElementById("point-of-customer").innerHTML = "...";
     document.getElementById("discount-checkbox").checked =
       currentBill.use_point == false;
+    document.getElementById("discount-checkbox").classList.add("d-none");
     document.getElementById("phone-no-input").value = "";
   }
 }
